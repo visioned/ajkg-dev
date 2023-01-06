5ad99c345aa464c214add4d15586dc538aee44c1
visioned <ajkg11@gmail.com>
2023-01-06 05:52:39 -0800

    asdf

--- components/Tools.js
+++ components/Tools.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Image from 'next/image';
 import tools from '../pages/api/data.json';
+import spiral from '../public/images/.svg';
 
 const Index = () => {
   return (
@@ -10,13 +11,27 @@ const Index = () => {
       <p className='py-2 font-karla flex justify-center tracking-wide'>
         its a playground
       </p>
-
+      <Image
+        src={spiral}
+        style={{
+          width: '100%',
+          height: '85%',
+          position: 'relative',
+          objectFit: 'fill',
+          left: '0px',
+          top: '0px',
+          bottom: '0px',
+          right: '0px',
+        }}
+        alt='...'
+        className='opacity-30 dark:opacity-5 z-0 '
+      />
       {/* column */}
       <div className='flex flex-wrap pt-5 gap-4 justify-center'>
         {tools.map((tool) => {
           return (
-            // card
-            <div className='z-10 dark:bg-[#3e3f4b] bg-gray-100 w-[110px] h-[130px] shadow-md hover: hover:drop-shadow-[0_11px_11px_rgba(252,255,0,0.09)] rounded-2xl hover:scale-110 transition ease-in duration-100 focus:' >
+            // card drop-shadow-[0_11px_11px_rgba(252,255,0,0.09)]
+            <button className='z-10 dark:bg-[#3e3f4b] bg-gray-100 w-[110px] h-[130px] shadow-md hover:shadow-xl rounded-2xl hover:scale-110 transition top-0.5 ease-in duration-100'>
               {/* card // icon + text*/}
               <div className='flex my-3 justify-center drop-shadow '>
                 <Image
@@ -29,7 +44,7 @@ const Index = () => {
               </div>
 
               <p className='flex justify-center font-karla'>{tool.title}</p>
-            </div>
+            </button>
           );
         })}
       </div>
